ae38e26b6d6606c4c48a18ce22d408e8b176daf5
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-11-01 20:24:59 -0400

    fixed problems in practice labs

---
+++ introductions.py
@@ -0,0 +1,6 @@
+## Write a function that takes 2 pieces of input and introduces them to one another
+
+name1 = input("Person 1, What is your name?")
+name2 = input("Person 2, What is your name?")
+
+print(name1 , " meet " , name2)
