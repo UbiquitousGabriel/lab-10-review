3baa0249126b1cf619cea1579f464f536c4316a0
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-11-01 20:41:34 -0400

    fix pt1

--- introductions.py
+++ introductions.py
@@ -1,6 +1,9 @@
 ## Write a function that takes 2 pieces of input and introduces them to one another
 
+def introduction(name1, name2):
+    print(name1, " meet ", name2)
+
 name1 = input("Person 1, What is your name?")
 name2 = input("Person 2, What is your name?")
 
-print(name1 , " meet " , name2)
+introduction(name1,name2)
